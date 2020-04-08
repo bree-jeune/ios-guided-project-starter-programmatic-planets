4ce2578c72246e2d3dc55d135a07593518325bcf
Breanna Young <bree.j.32@gmail.com>
2020-04-07 23:40:05 -0400

    Programmatic UI Training Kit Guided Project

--- ProgrammaticPlanets/Views/PlanetCollectionViewCell.swift
+++ ProgrammaticPlanets/Views/PlanetCollectionViewCell.swift
@@ -19,10 +19,87 @@ class PlanetCollectionViewCell: UICollectionViewCell {
         }
     }
     
+    override init(frame: CGRect) {
+        super.init(frame: frame)
+        setUpSubViews()
+    }
+    
+    required init?(coder: NSCoder) {
+        super.init(coder: coder)
+        setUpSubViews()
+    }
+    
     private func updateViews() {
         guard let planet = planet else { return }
         imageView.image = planet.image
         nameLabel.text = planet.name
+    }
+    
+    private func setUpSubViews() {
+        imageView.translatesAutoresizingMaskIntoConstraints = false
+        imageView.contentMode = .scaleAspectFit
+        
+        addSubview(imageView)
+        
+        
+        // This is the Y position
+        NSLayoutConstraint(item: imageView,
+                           attribute: .top,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .top,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        
+        // This is the X position
+        NSLayoutConstraint(item: imageView,
+                           attribute: .leading,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .leading,
+                           multiplier: 1,
+                           constant: 4).isActive = true
+        
+        // This is the width (left and up are both negative**)
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .trailing,
+                           relatedBy: .equal,
+                           toItem: self,
+                           attribute: .trailing,
+                           multiplier: 1,
+                           constant: -4).isActive = true
+        
+        // This is the height
+        
+        NSLayoutConstraint(item: imageView,
+                           attribute: .height,
+                           relatedBy: .equal,
+                           toItem: imageView,
+                           attribute: .width,
+                           multiplier: 1,
+                           constant: 0).isActive = true
+        
+        
+        // Label constraints
+        
+        nameLabel.translatesAutoresizingMaskIntoConstraints = false
+        nameLabel.textAlignment = .center
+        addSubview(nameLabel)
+        
+        // This is the Y position
+        
+        nameLabel.topAnchor.constraint(equalTo: imageView.bottomAnchor,
+                                       constant: 4).isActive = true
+        
+        // This is the X position
+        nameLabel.leadingAnchor.constraint(equalTo: leadingAnchor,
+                                           constant: 2).isActive = true
+        
+        // This is the width
+        nameLabel.trailingAnchor.constraint(equalTo: trailingAnchor,
+                                            constant: -2).isActive = true
+        
     }
     
 }
